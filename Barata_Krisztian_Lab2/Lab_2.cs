c453803aa9e8303e6eb4020b7ca9e8d27169721c
Krisztian Barata <iilexlevisii@gmail.com>
2020-10-30 17:14:03 +0200

    Add project files.

---
+++ Barata_Krisztian_Lab2/Lab_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barata_Krisztian_Lab2
+{
+    class Lab_2
+    {
+    }
+}
---
+++ Barata_Krisztian_Lab2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barata_Krisztian_Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtVanillaFilled_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txt_Price_Current_Sale_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
